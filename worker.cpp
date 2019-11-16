d8460a321e57588177384acb42d270bc160c00e5
RPG-Rebel <55595199+RPG-Rebel@users.noreply.github.com>
2019-11-15 19:58:46 -0500

    setter and getter for worker
    setter and getter for worker

--- worker.cpp
+++ worker.cpp
@@ -1,2 +1,28 @@
 //worker.cpp
 #include"worker.h"
+
+Worker::Worker()
+{
+	setExp(0);
+	setPay(0);
+	setSec(0);
+}
+
+void Worker::setExp(int exp)
+{
+	this ->exp = exp;
+}
+
+void Worker::setPay(double pay)
+{
+	this ->pay = pay;
+}
+
+void Worker::setSec(int security)
+{
+	this ->security = security;
+}
+
+int Worker::getExp() const{return exp;}
+double Worker::getPay() const{return pay;}
+int Worker::setSec() const{return security;}
--- worker.h
+++ worker.h
@@ -3,14 +3,21 @@
 #define WORKER_H
 
 public:
-
-
-
+	//constructors
+	Worker();
+	Worker(int, double, int);
+	//mutators
+	void setExp();
+	void setPay();
+	void setSec();
+	//accessors
+	int getExp() const;
+	double getPay() const;
+	int getSec() const;
 private:
 	int security;
 	double pay;
 	int exp;// experiance
-
 protected:
 
 
