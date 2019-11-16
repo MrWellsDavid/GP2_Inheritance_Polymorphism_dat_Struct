221201e5b425554e1b6a8ac8b256e88bd019a6f5
Barron Botts <barronbotts@gmail.com>
2019-11-15 20:14:07 -0500

    CONFLICTS IN NAMING
    all headerfiles have lowercase names, but some people are used to uppercase first letters.

--- worker.cpp
+++ worker.cpp
@@ -1,28 +1,28 @@
 //worker.cpp
 #include"worker.h"
-
-Worker::Worker()
+using namespace std;
+worker::Worker()
 {
 	setExp(0);
 	setPay(0);
 	setSec(0);
 }
 
-void Worker::setExp(int exp)
+void worker::setExp(int exp)
 {
 	this ->exp = exp;
 }
 
-void Worker::setPay(double pay)
+void worker::setPay(double pay)
 {
 	this ->pay = pay;
 }
 
-void Worker::setSec(int security)
+void worker::setSec(int security)
 {
 	this ->security = security;
 }
 
-int Worker::getExp() const{return exp;}
-double Worker::getPay() const{return pay;}
-int Worker::setSec() const{return security;}
+int worker::getExp() const{return exp;}
+double worker::getPay() const{return pay;}
+int worker::getSec() const{return security;}
--- worker.h
+++ worker.h
@@ -2,35 +2,25 @@
 #ifndef WORKER_H
 #define WORKER_H
 
-public:
-	//constructors
-	Worker();
-	Worker(int, double, int);
-	//mutators
-	void setExp();
-	void setPay();
-	void setSec();
-	//accessors
-	int getExp() const;
-	double getPay() const;
-	int getSec() const;
-private:
-	int security;
-	double pay;
-	int exp;// experiance
-protected:
-
 class worker {
 	public:
-		
+		//constructors
+		Worker();
+		Worker(int, double, int);
+		//mutators
+		void setExp(int);
+		void setPay(double);
+		void setSec(int);
+		//accessors
+		int getExp() const;
+		double getPay() const;
+		int getSec() const;
 	private:
 		int security;
 		double pay;
 		int exp;// experiance
-	
 	protected:
-	
-
+		
 };
 
 #endif
