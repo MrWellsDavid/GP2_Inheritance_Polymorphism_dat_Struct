99da0cae59b6fc3256e0b3fc4f86ef3099bff594
Barron Botts <barronbotts@gmail.com>
2019-12-15 14:18:18 -0500

    Manager and employee
    Both created, no tests.

--- man.h
+++ man.h
@@ -1,12 +1,19 @@
 //man.h
 
+#include"worker.h"
 using namespace std;
 
-class man {
+#ifndef MAN_H
+#define MAN_H
+
+class man: public worker{
 	public:
+		//Constructors
+		man();
+		man(string, string, int, int, double);
 		
-	private:
-		
-	protected:
-		
+		//Accessors
+		string getIDString();
 };
+
+#endif
