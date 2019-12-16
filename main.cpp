0cbe5704b298d6db5cdcc43a1839fbf8ac193186
thetruemalkavian <mr.wellsdavid@gmail.com>
2019-12-16 16:01:46 -0500

    // login menu

--- main.cpp
+++ main.cpp
@@ -12,7 +12,85 @@
 #include "user.h"
 #include "worker.h"
 
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+using namespace std; 
+
+
+int findUsr(user**, int, string);
+bool startMenu(user**, int&, int);
+string loginMenu();
+bool checkPWord(user**, string, int);
+
+
+string loginMenu() {
+	string uName, pWord;
+	cout << "/t==================== Login ===================="
+		<< "\n\n\tUsername: ";
+	cin >> uName;
+	cout << "\n\tPassword: ";
+	cin >> pWord;
+	return pWord;
+}
+
+bool startLogin(user** uPTR, int& log, int usr) { // usr is the number of people in the system to check against
+	int pos;
+	string uName, pWord;
+	bool access = false;
+	int denyCount = 3; // Number of times that someone can enter a wrong password
+
+	cout << "\n Login"
+		<< "User Name:\t";
+	cin >> uName;
+	pos = findUsr(uPTR, usr, uName);
+	if (pos != -1) {
+		cout << "\n \t Password: ";
+		cin >> pWord;
+		access = checkPWord(uPTR, pWord, pos);
+		do {
+			if (access == true) {
+				cout << "\n\n\n \t Welcome.\n\n\n";
+				system("PAUSE");
+				log = pos;
+				return true;
+			}
+			else {
+				cout << "DENIED\n"
+					<< "you have tried:" << denyCount << "attemps MAY be limited"
+					<< "enter the password you now remember:";
+				cin >> pWord;
+				denyCount++;
+			}
+			access = checkPWord(uPTR, pWord, pos);
+		} while (denyCount <= 5);
+		if (access == true) {
+			cout << "\n\n\n \t Welcome.\n\n\n";
+			system("PAUSE");
+			log = pos;
+			return true;
+		}
+	}
+	else
+		cout << " \nuser not found\n";
+	return false;
+}
+int findUsr(user** uPTR, int usrnum, string uName) {//usrnum is the size of the users vector
+	for (int i = 0; i < usrnum; i++) {
+		if ((*uPTR)->getName() == uName)
+			return i;
+		uPTR++;
+	}
+	return-1;
+}
+bool checkPWord(user** uPTR, string pass, int pos) {
+	if ((*uPTR[pos]).getPWord() == pass)
+		return true;
+	return false;
+}
 int main(int argc, char** argv) {
-	
+	loginMenu();
 	return 0;
 }
