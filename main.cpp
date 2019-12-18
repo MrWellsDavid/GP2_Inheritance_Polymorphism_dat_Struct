e525e5fc77c382af07ca0822efa62d3054a3f325
RPG-Rebel <55595199+RPG-Rebel@users.noreply.github.com>
2019-12-17 20:01:24 -0500

    report
    test

--- main.cpp
+++ main.cpp
@@ -25,11 +25,12 @@ int menu()
 	cout << "\t \t \t Main \n"
 		 << "1. Log Equipment in\n"
 		 << "2. View Current Status\n"
-		 << "3. Stop Work\n"
+		 << "3. Create Customer Ticket"
 		 << "4. Certify(for manager use only)\n"
 		 << "5. View Certified Ticket\n"
 		 << "6. Create Employee\n"
 		 << "7. View Employees\n"
+		 << "8. Create Report\n"
 		 << "Enter choice: ";
 	cin >> choice;
 	return choice;
@@ -102,6 +103,37 @@ void makeEmp()
 	writeToFile(first, last, id, exp);
 }
 
+void writeToFile3(string first, string last, int id)
+{
+	int count = 1;
+	ofstream outData;
+	outData.open("Cus.txt", ios::app);
+	for (int i =0; i < count; i++)
+	{
+		outData << first << " " << last << " " << id << endl;
+	}
+	
+	outData.close();
+}
+
+cus* makeCus() 
+{
+	string first, last;
+	int id;
+
+	cout <<"\t \t \t Custumer \n"
+		 <<"Enter first name: \n";
+	cin >> first;
+	cout <<"Enter last name: \n";
+	cin >> last;
+	cout <<"Enter your ID: \n";
+	cin >> id;
+	
+	cus* cPTR = new cus(first, last, id);
+	writeToFile3(first, last, id);
+	return cPTR;
+}
+
 void viewEmp()
 {
 	string line;
@@ -135,6 +167,7 @@ void viewStat()
 int main(int argc, char** argv) 
 {
 	int option;
+	cus* cPTR;
 	int rec = 0;
 	
 	do
@@ -151,6 +184,7 @@ int main(int argc, char** argv)
 				viewStat();
 				break;
 			case 3:
+				cPTR = makeCus();
 				break;
 			case 4:
 				break;
@@ -162,11 +196,17 @@ int main(int argc, char** argv)
 			case 7:
 				viewEmp();
 				break;
+			case 8:
+				report* reporting();
+				system("CLS");
+//				cout << reporting -> consumerReport(cPTR) << endl;
+				system("PAUSE");
+				break;
 			default : cout <<"Invalide choice!\n";	
 		}
 		system("PAUSE");
 		
-	}while(option != 8);
+	}while(option != 9);
 	
 	return 0;
 }
